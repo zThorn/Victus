50f2ab2771619a071128e19a50f6e5589ee7211a
SDFortier <stevendfortier@yahoo.com>
2015-03-05 18:29:10 -0500

    Basic version of position saving.

--- core/src/com/TEAM_NAME/Fructus/FirstPersonCamera.java
+++ core/src/com/TEAM_NAME/Fructus/FirstPersonCamera.java
@@ -2,6 +2,7 @@ package com.TEAM_NAME.Fructus;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.g3d.utils.FirstPersonCameraController;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.math.Vector3;
@@ -19,6 +20,8 @@ public class FirstPersonCamera extends FirstPersonCameraController {
     private int BACKWARD = Input.Keys.S;
     private int UP = Input.Keys.Q;
     private int DOWN = Input.Keys.E;
+    private int SAVE = Input.Keys.K;
+    private int LOAD = Input.Keys.L;
     private float velocity = 5;
     private float degreesPerPixel = 0.5f;
     private final Vector3 tmp = new Vector3();
@@ -50,6 +53,25 @@ public class FirstPersonCamera extends FirstPersonCameraController {
         super.update(deltaTime);
         camera.position.y = 0;
         camera.update(true);
+        if(Gdx.input.isKeyPressed(SAVE)) {
+        	save();
+        }
+        if(Gdx.input.isKeyPressed(LOAD)) {
+        	load();
+        }
     }
+	public void save(){	 
+		Preferences prefs = Gdx.app.getPreferences("Fructus_Victus");
+		prefs.putFloat("camerax", camera.position.x);
+		prefs.putFloat("cameray", camera.position.y);
+		prefs.putFloat("cameraz", camera.position.z);
+		prefs.flush();
+	}
+	public void load(){
+		Preferences prefs = Gdx.app.getPreferences("Fructus_Victus");
+		camera.position.x = prefs.getFloat("camerax");
+		camera.position.y = prefs.getFloat("cameray");
+		camera.position.z = prefs.getFloat("cameraz");
+	}
 
 }
