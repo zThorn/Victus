b9c1ebdc4e75f6c2ff97d8b47984adb07cbd594f
Zach Thornton <thornotnz@wit.edu>
2015-08-24 17:18:49 -0400

    Should have done this awhile ago, but I fixed a file with incorrect character encoding

--- core/src/com/TEAM_NAME/Fructus/Story.java
+++ core/src/com/TEAM_NAME/Fructus/Story.java
@@ -8,27 +8,27 @@ public class Story {
 	public static ArrayList<String> storyItems = new ArrayList<String>();
 	
 	public static void loadStory(){
-		storyItems.add("This is a nightmare. I knew it would be bad but I could never have imagined this.\n If I hadn’t written down my mission before I entered site of the fruitation I feel\n I could have ended up walking these sticky halls for an eternity.\n But there is still some hope at least, if I can just reach the lab I can fix everything. \nIf I can just get back to the Heart and cover it with the solution I made just before\n the evacuation. What cowardice I showed on that day…");
+		storyItems.add("This is a nightmare. I knew it would be bad but I could never have imagined this.\n If I hadnâ€™t written down my mission before I entered site of the fruitation I feel\n I could have ended up walking these sticky halls for an eternity.\n But there is still some hope at least, if I can just reach the lab I can fix everything. \nIf I can just get back to the Heart and cover it with the solution I made just before\n the evacuation. What cowardice I showed on that dayâ€¦");
 				
 	}
 }
 /*Prologue:
 Donny Tribbins awakens on the floor of the first fruit, alone and amnesic.
-“W…where am I?”
-………………………
-“It seems like I’m inside…some kind of…oh god- it’s fruit!”
-“What am I doing here? I need to get moving, my clothes are already gross with fruit-goo”
+â€œWâ€¦where am I?â€
+â€¦â€¦â€¦â€¦â€¦â€¦â€¦â€¦â€¦
+â€œIt seems like Iâ€™m insideâ€¦some kind ofâ€¦oh god- itâ€™s fruit!â€
+â€œWhat am I doing here? I need to get moving, my clothes are already gross with fruit-gooâ€
 Proceed to gameplay, next story part begins at first journal find.
 Story Piece #1:
-“Dr. Gormak –PHD. Day 1
-This is a nightmare. I knew it would be bad but I could never have imagined this. If I hadn’t written down my mission before I entered site of the fruitation I feel I could have ended up walking these sticky halls for an eternity. But there is still some hope at least, if I can just reach the lab I can fix everything. If I can just get back to the Heart and cover it with the solution I made just before the evacuation. What cowardice I showed on that day…”
+â€œDr. Gormak â€“PHD. Day 1
+This is a nightmare. I knew it would be bad but I could never have imagined this. If I hadnâ€™t written down my mission before I entered site of the fruitation I feel I could have ended up walking these sticky halls for an eternity. But there is still some hope at least, if I can just reach the lab I can fix everything. If I can just get back to the Heart and cover it with the solution I made just before the evacuation. What cowardice I showed on that dayâ€¦â€
 Story Piece #2:
-“Dr. Gormak—PHD. Day? 2
-Days? Why days? I have no demarcation of time here inside the fruit. Perhaps I should write it down so that I don’t forget. It all happened so fast. We had just enough time to evacuate before the lab was surrounded by giant, mutated fruit. All shape was lost, but many of the other properties were retained…writing helps, but I find my mind wandering again even now. My theory is that there are pockets of gas, and pockets of air, allowing moments of clarity in this sea of confusion…..”
+â€œDr. Gormakâ€”PHD. Day? 2
+Days? Why days? I have no demarcation of time here inside the fruit. Perhaps I should write it down so that I donâ€™t forget. It all happened so fast. We had just enough time to evacuate before the lab was surrounded by giant, mutated fruit. All shape was lost, but many of the other properties were retainedâ€¦writing helps, but I find my mind wandering again even now. My theory is that there are pockets of gas, and pockets of air, allowing moments of clarity in this sea of confusionâ€¦..â€
 Story Piece #3:
-“Dr….Gorman? –Phd Day? What day is it? Ha!
-I laugh at the concept of time in the Maze, as I have come to call it. Will I ever find the lab? That’s all I can remember, “find the lab” and remember really isn’t the word. I wrote it on my arm because I keep leaving my notes behind when I get up and wander off. I wonder if anyone will ever read this? DONNY! I REMEMBER! DONNY IF YOU READ THIS, GET TO THE LAB, I HAVE FAITH IN YOU!”
+â€œDrâ€¦.Gorman? â€“Phd Day? What day is it? Ha!
+I laugh at the concept of time in the Maze, as I have come to call it. Will I ever find the lab? Thatâ€™s all I can remember, â€œfind the labâ€ and remember really isnâ€™t the word. I wrote it on my arm because I keep leaving my notes behind when I get up and wander off. I wonder if anyone will ever read this? DONNY! I REMEMBER! DONNY IF YOU READ THIS, GET TO THE LAB, I HAVE FAITH IN YOU!â€
 Story Piece #4: Final
 Too slow ending:
-“Who am I? Why am I here? I laugh and laugh but I don’t remember what’s funny. I’m so tired of eating fruit, what I wouldn’t give for a proper last meal. How long have I wandered within the fruit?! Alas, I make it this far and no further. I stare at the vial, but I cannot remember its significance. I feel a terrible presence in this room. A room! I’m not in fruit anymore? Or at least, not all the way… Even as I write I am consumed with fits of laughter, uncontrollable laughter, this is surely the edge of madness. There is a terrible presence here”
+â€œWho am I? Why am I here? I laugh and laugh but I donâ€™t remember whatâ€™s funny. Iâ€™m so tired of eating fruit, what I wouldnâ€™t give for a proper last meal. How long have I wandered within the fruit?! Alas, I make it this far and no further. I stare at the vial, but I cannot remember its significance. I feel a terrible presence in this room. A room! Iâ€™m not in fruit anymore? Or at least, not all the wayâ€¦ Even as I write I am consumed with fits of laughter, uncontrollable laughter, this is surely the edge of madness. There is a terrible presence hereâ€
 */
